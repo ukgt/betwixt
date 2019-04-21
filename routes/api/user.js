18949a363c8817e93c7c5466323a6fc3a2315414
Gunslinger <jtgeislinger@gmail.com>
2019-04-20 20:08:32 -0400

    initial setup

---
+++ routes/api/user.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+
+// User Model
+const User = require("../../models/Users");
+
+// GET route for api/user
+// GET all Users
+// Authentication???
+router.get('/', (req, res) => {
+     User.find().then(user => res.json(user))
+}); 
+
+// POST route fo api/user
+// Create a Post
+// Authentication???
+router.post("/", (req, res) => {
+     const newUser = new User({
+          name: req.body.name
+     });
+     newUser.save().then(user => res.json(user));
+});
+
+// //DELETE api/user/:id
+// //DELETE a User
+// router.delete("/:id", (req, res) => {
+//      User.findById(req.params.id)
+//           .then(item => item.remove().then(() => res.json({success: true})))
+//           .catch(err => res.status(404).json({ success: false}))
+// });
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,34 @@
+//Dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const user = require("./routes/api/user")
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+//Bodyparser Middleware
+app.use(bodyParser.json());
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Add routes, both API and view
+app.use('/api/user', user);
+
+// Connect to the Mongo DB
+mongoose
+    .connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist")
+    .then(() => console.log("MongoDB Connected..."))
+    .catch((err => console.log(err)));
+
+
+// Start the API server
+app.listen(PORT, () => console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`));
