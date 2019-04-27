5379ce349b0e62b2483713ae067cdcfa1f7a21b2
Linu Ramachandran <linuram@gmail.com>
2019-04-27 12:39:21 -0400

    updated index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,18 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+let state = {}
+window.setState = changes => {
+  state = Object.assign({}, state, changes)
+  ReactDOM.render(<App {...state} />, document.getElementById('root'))
+}
+
+let initialState = {
+  name: 'John',
+  location: window.location.pathname.replace(/^\/?|\/$/g, '')
+}
+window.setState(initialState)
+
 ReactDOM.render(
   <Router>
     <App />
