48446d72bcfcee3ef6841879004afbf4d861919e
ukgt <ukgt2019@gmail.com>
2019-05-02 19:31:41 -0400

    heroku deploy files

--- server.js
+++ server.js
@@ -98,5 +98,13 @@ mongoose
     .catch((err => console.log(err)));
 
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 // Start the API server
 app.listen(PORT, () => console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`));
