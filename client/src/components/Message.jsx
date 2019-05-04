3bcb10c94cd73aea01c91baeeebf17d330ec3f60
Linu Ramachandran <linuram@gmail.com>
2019-05-04 12:26:13 -0400

    updated chatlist

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,49 +1,49 @@
-import React from "react";
-import Pusher from "pusher-js";
-import ChatBox from "../ChatBox";
-import ChatList from "../ChatList";
-import axios from "axios";
-
+import React from 'react'
+import Pusher from 'pusher-js'
+import ChatBox from '../ChatBox'
+import ChatList from '../ChatList'
+import axios from 'axios'
 
 export default class Message extends React.Component {
   state = {
     checkingSession: true,
-    text: "",
-    username: "",
+    text: '',
+    username: '',
     chats: []
-  };
-  async componentDidMount() {
-    this.setState({ checkingSession: false });
-    const username = window.prompt("Username: ", "Anonymous"); // TODO: 
-    this.setState({ username, checkingSession: false });
-    const pusher = new Pusher("be45f6d9f5b297267413", {
-      cluster: "us2",
+  }
+  async componentDidMount () {
+    this.setState({ checkingSession: false })
+    const username = window.prompt('Username: ', 'Anonymous') // TODO:
+    this.setState({ username, checkingSession: false })
+    const pusher = new Pusher('be45f6d9f5b297267413', {
+      cluster: 'us2',
       encrypted: true
-    });
-    const channel = pusher.subscribe("chat");
-    channel.bind("message", data => {
-      this.setState({ chats: [...this.state.chats, data], test: "" });
-    });
+    })
+    const channel = pusher.subscribe('chat')
+    channel.bind('message', data => {
+      this.setState({ chats: [...this.state.chats, data], test: '' })
+      console.log(this.state)
+    })
   }
   handleTextChange = e => {
     if (e.keyCode === 13) {
       let payload = {
-        userId: sessionStorage.getItem("currentUser"),
+        userId: sessionStorage.getItem('currentUser'),
         message: this.state.text
-      };
-      console.log("hey!!!" , payload);
-      axios.post("/message", payload);
+      }
+      console.log('hey!!!', payload)
+      axios.post('/message', payload)
     } else {
-      this.setState({ text: e.target.value });
+      this.setState({ text: e.target.value })
     }
-  };
+  }
 
-  render() {
+  render () {
     // JSX
     return (
       <div>
         <section>
-          <ChatList chats={this.state.chats} />
+          <ChatList chats={this.state.chats} username={this.state.username}/>
           <ChatBox
             text={this.state.text}
             username={this.state.username}
@@ -51,6 +51,6 @@ export default class Message extends React.Component {
           />
         </section>
       </div>
-    );
+    )
   }
 }
