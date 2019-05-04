051a2cb33fd14718ecf06d73312878aeca716787
Shaun <torianharris917@gmail.com>
2019-05-04 11:07:21 -0400

    apis merged

--- client/src/Auth.js
+++ client/src/Auth.js
@@ -8,7 +8,7 @@ class Auth {
       domain: "dev-1f5flisp.auth0.com",
       audience: "https://dev-1f5flisp.auth0.com/userinfo",
       clientID: "mZHUe42c4yE0R1nyFzcoMGss5PwoeESZ",
-      redirectUri: "https://betwixt-gt.herokuapp.com/callback",
+      redirectUri: process.env.REDIRECT_URL || "https://betwixt-gt.herokuapp.com/callback",
       responseType: "id_token",
       scope: "openid profile",
     });
